96ff379097a281f60650514d3aa9589395fb9710
DongOk <hcan159@naver.com>
2023-12-28 21:34:01 +0900

    @add layout

---
+++ src/app/components/layout.js
@@ -0,0 +1,8 @@
+export default function LayoutRouter({ childern }) {
+  return (
+    <>
+      <h1>레이아웃</h1>
+      <div>{childern}</div>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1> 오늘도 빡코딩</h1>
+      <h1> 홈입니다.</h1>
     </div>
   );
 }
