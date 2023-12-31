2194969e19b4932ca1602afe230d043f2383df7d
DongOk <hcan159@naver.com>
2023-12-31 23:42:20 +0900

    @add layout Home

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "빡코딩 포트폴리오",
-  description: "오늘도 빡코딩",
+  title: "이끽기 포트폴리오",
+  description: "오늘도 열공",
 };
 
 export default function RootLayout({ children }) {
