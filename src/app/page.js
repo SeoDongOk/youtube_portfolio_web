311b79ac16952c916d3d2f12cb38629e8f7e6320
DongOk <hcan159@naver.com>
2024-01-04 18:18:50 +0900

    @add icon

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,23 @@
 import Hero from "./components/Home/hero";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import { ThemeProvider } from "next-themes";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <div
-          className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
-          bis_skin_checked="1"
-        >
-          <Hero />
-        </div>
-      </section>
-      <Footer />
-    </div>
+    <ThemeProvider attribute="class">
+      <div>
+        <Header />
+        <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+          <div
+            className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
+            bis_skin_checked="1"
+          >
+            <Hero />
+          </div>
+        </section>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
