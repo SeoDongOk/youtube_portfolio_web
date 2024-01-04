bdef2e7923653e50913cf319bf0b4289e9c41d4a
DongOk <hcan159@naver.com>
2024-01-05 00:14:15 +0900

    @fix css/darkmode

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -1,18 +1,22 @@
+"use client";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import { ThemeProvider } from "next-themes";
 export default function Home() {
   return (
     <div>
-      <Header />
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <div
-          className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
-          bis_skin_checked="1"
-        >
-          <h1>프로젝트</h1>
-        </div>
-      </section>
-      <Footer />
+      <ThemeProvider>
+        <Header />
+        <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+          <div
+            className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
+            bis_skin_checked="1"
+          >
+            <h1>프로젝트</h1>
+          </div>
+        </section>
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 }
